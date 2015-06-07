60bd9a2c6075280c71db186a78370df41e26b502
marco <marco@marcos-MacBook-Pro.local>
2015-06-07 19:47:55 -0300

    Added Plot3

--- plot3.R
+++ plot3.R
@@ -1,8 +1,8 @@
-#********** plot2.R: Global Active Power per day names ************
+#********** plot3.R: Sub-meterings  ************
 #Reading with read.table will get all the rows first, takes too much time
 #SQLDF faster @ this time, read and filter at ~ same time
 
-#Required library for running this plot2.R
+#Required library for running this plot3.R
 pkgReady <- function(pkgName) is.element(pkgName, installed.packages()[,1])
 if(!pkgReady('sqldf')){install.packages('sqldf')}
 library(sqldf)
@@ -23,9 +23,13 @@ pwrCons[,'Date'] = as.Date(pwrCons[,'Date'], format = '%d/%m/%Y')
 fDateTime<-as.POSIXct(paste(pwrCons$Date, pwrCons$Time))
 
 #Plotting
-png(filename = 'plot2.png', width = 480, height = 480)
-plot(fDateTime, pwrCons$Global_active_power, type="l", xlab = '',
-     ylab = 'Global Active Power(kilowatts)')
+png(filename = 'plot3.png', width = 480, height = 480)
+plot(fDateTime, pwrCons$Sub_metering_1, type="l",col=c('black'),
+  ylab = 'Energy sub metering')
+lines(fDateTime, pwrCons$Sub_metering_2, type="l",col=c('red'))
+lines(fDateTime, pwrCons$Sub_metering_3, type="l",col=c('blue'))
+legend('topright', pch=1, col=c('black', 'red', 'blue'),
+  legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3') )
 dev.off()
 
 closeAllConnections()
